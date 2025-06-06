df131c1156aaff68c22421d4d85699ef9f04329d
Banaan Kiamanesh <kiamaneshbanaan@gmail.com>
2025-06-07 00:14:25 +0200

    Robust Tracking Done!

---
+++ Lab 1/2 Laboratory  Assignment - Numerical Experiments/2.2 Position State Space Control Design/Robust Tracking Controller Design with Integral Action/Design.m
@@ -0,0 +1,69 @@
+clear
+close all
+clc
+
+addpath("../../../utils/");
+
+%% Model Parameter Declaration
+params;
+load BBParams.mat
+
+%% Simplified Plant Model
+Req = mot.R + sens.curr.Rs;
+Jeq = Jeq_hat;
+
+Km = drv.dcgain*mot.Kt / (Req * Beq + mot.Kt*mot.Ke);
+Tm = Req * Jeq / (Req * Beq + mot.Kt*mot.Ke);
+
+% Simplified System State Space Model
+A = [0, 1; 0, -1/Tm];
+B = [0; Km/Tm/gbox.N];
+C = [1, 0];
+D = 0;
+
+N = size(A, 1);             % System Order
+
+%% Augmented System
+Ae = [0, C; zeros(2, 1), A];
+Be = [0; B];
+Ce = [0, C];
+
+% N = size(Ae, 1);             % Augmented System Order
+
+%% Controller Params
+SettlingTime    = 0.15;           % Settling Time
+Mp              = 0.1;            % Overshoot
+
+% Approximated Second Order Sys Params
+Zeta = log(1/Mp) / sqrt(pi^2 + log(1/Mp)^2);
+Wn   = 3 / Zeta / SettlingTime;
+
+% Controller Desired Poles
+L1 = -Zeta * Wn + 1j * Wn * sqrt(1-Zeta^2);
+L2 = conj(L1);
+L3 = -Zeta * Wn;
+
+Poles = [L1, L2, L3];
+
+%% State Feedback Controller Design
+K = place(Ae, Be, Poles);
+
+Ki = K(1);
+K  = K(2:end);
+
+% Computing FeedForward Gains
+tmp = [A, B; C, 0] \ [zeros(N, 1); 1];
+Nx = tmp(1:N, 1);
+Nu = tmp(end);
+
+Nr = Nu + K * Nx;
+
+%% Reference Signal
+Ref = 70;
+
+%% Real Derivative
+Delta = 1 / sqrt(2);
+Wc    = 2 * pi * 50;
+
+RD.num = [Wc^2, 0];
+RD.den = [1, 2*Delta*Wc, Wc^2];
