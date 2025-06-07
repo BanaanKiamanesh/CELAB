64023d7f3ada61bd3aa8c3ce9420a9be47789161
Banaan Kiamanesh <kiamaneshbanaan@gmail.com>
2025-06-07 18:26:34 +0200

    LAB2 FeedForward PID Added

---
+++ Lab 2/2 Laboratory Assignments - Numerical Simulations/2.1 Design by Emulation/2.1.1 Position PID Controller/Feed Forward PID/Design.m
@@ -0,0 +1,56 @@
+clear
+close all
+clc
+
+addpath("../../../../utils/");
+
+%% Model Parameter Declaration
+params;
+load BBParams.mat
+
+%% Simplified Plant Model
+Req = mot.R + sens.curr.Rs;
+% Jeq = mot.J + mld.JD/gbox.N^2;
+Jeq = Jeq_hat;
+
+Km = drv.dcgain*mot.Kt / (Req * Beq + mot.Kt*mot.Ke);
+Tm = Req * Jeq / (Req * Beq + mot.Kt*mot.Ke);
+
+% Simplified System Trans Data
+SS.num = Km;
+SS.den = [Tm * gbox.N, gbox.N, 0];
+P = tf(SS.num, SS.den);
+
+%% Controller Params
+Alpha           = 40;             % Ti / Td  ratio
+SettlingTime    = 0.15;           % Settling Time
+Mp              = 0.1;            % Overshoot
+
+% Discrete Simulation Specs
+Ts = 1e-3;
+
+%% PID Controller Design
+[Kp, Ki, Kd, Tl, Wgc] = DesignPIDBode(P, SettlingTime, Mp, Alpha);
+
+% Integrator Part Transfer Function
+ITF.num = [Ts, 0];
+ITF.den = [1, -1];
+
+% Derivative Part Transfer Function
+DTF.num = [1, -1];
+DTF.den = [Tl+Ts, -Tl];
+
+%% FeedForward Compensation Parameters
+% Inertia Compensation
+I_C = (gbox.N * Req * Jeq_hat)/(drv.dcgain * mot.Kt);
+
+% Friction Compensation
+F_C = Req/(drv.dcgain * mot.Kt * gbox.N);
+
+% BEMF Compensation
+B_C = (gbox.N * mot.Ke)/drv.dcgain;
+
+%% Reference Signal
+
+% Periodic Acceleration Reference
+a_l_ref = [900, 0, -900, -900, 0, 900];
