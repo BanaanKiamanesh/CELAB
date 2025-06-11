71810c203b8c3f49d5ddaf12f8379fed8ffde9ca
Banaan Kiamanesh <kiamaneshbanaan@gmail.com>
2025-06-11 10:45:26 +0200

    Simple Example of Extended Estimator Added

---
+++ Sample Exam/Some Other Controllers/Extended Estimator/Main.m
@@ -0,0 +1,50 @@
+clear
+close all
+clc
+
+%% Parameters
+A = [0, 1; 0, -20];
+B = [0; 60];
+C = [1, 0];
+D = 0;
+
+%% Design to Track a Sine Wave and a Step
+% Input System
+W = 6;
+Ap = [0, 1, 0; 0, 0, 1; 0 -W^2, 0];
+Cp = [1, 0, 0];
+
+% Augmented System
+Ae = [Ap, zeros(3, 2); B * Cp, A];
+Be = [zeros(3, 1); B];
+Ce = [zeros(1, 3), C];
+
+% Pole Placement
+SettlingTime = 0.15;           % Settling Time
+Mp           = 0.1;            % Overshoot
+
+% Approximated Second Order Sys Params
+Zeta = log(1/Mp) / sqrt(pi^2 + log(1/Mp)^2);
+Wn   = 3 / Zeta / SettlingTime;
+
+% Desired Poles
+Lc1 = -Zeta * Wn + 1j * Wn * sqrt(1 - Zeta^2);
+Lc  = [Lc1, conj(Lc1)];
+
+K   = place(A, B, Lc);
+
+%% Observer Design
+Le1 =  5 * Wn * exp(1j*(-pi + pi/3));
+Le2 = conj(Le1);
+Le3 =  5 * Wn * exp(1j*(-pi + pi/6));
+Le4 = conj(Le3);
+Le5 = -5 * Wn;
+
+Le = [Le1, Le2, Le3, Le4, Le5];
+L = place(Ae', Ce', Le)'; 
+
+Ao = Ae - L*Ce;
+Bo = [Be, L];
+Co = eye(5);
+Do = zeros(5, 2);
+
